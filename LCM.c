1c989105ade34a89a340945a7984b204ed0775a0
chodisettijahnavi <97425832+chodisettijahnavi@users.noreply.github.com>
2022-04-28 20:12:42 +0530

    #include <stdio.h> int main() {     int n1,n2,i,gcd,lcm;     scanf("%d %d",&n1,&n2);     for (i = 1; i <= n1 && i <= n2; ++i) {         if(n1%i==0 && n2%i==0)             gcd= i;     }     lcm=(n1*n2)/gcd;     printf("%d",lcm);     return 0; }

--- LCM.c
+++ LCM.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 int main() {
- int a, b;
- scanf("%d", &a);
- scanf("%d", &b);
- int lcm = (a > b) ? a : b;
- while(1){
- if(lcm%a==0 && lcm%b==0)
- break;
- lcm++;
- }
- printf("%d",lcm);
- return 0;
+    int n1,n2,i,gcd,lcm;
+    scanf("%d %d",&n1,&n2);
+    for (i = 1; i <= n1 && i <= n2; ++i) {
+        if(n1%i==0 && n2%i==0)
+            gcd= i;
+    }
+    lcm=(n1*n2)/gcd;
+    printf("%d",lcm);
+    return 0;
 }
