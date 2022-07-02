549a793f52d0a7b39df2b14cdcb098dda8c694fd
chodisettijahnavi <97425832+chodisettijahnavi@users.noreply.github.com>
2022-07-02 18:04:03 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,c,sum=0,j;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(arr[i]==arr[j])             {                 c++;                 if(c==1)                 {                     sum=sum+arr[i];                 }                 if(j>i)                 {                     arr[j]=0;                 }             }         }     }     printf("%d",sum); }

---
+++ unique_sum.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,c,sum=0,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                c++;
+                if(c==1)
+                {
+                    sum=sum+arr[i];
+                }
+                if(j>i)
+                {
+                    arr[j]=0;
+                }
+            }
+        }
+    }
+    printf("%d",sum);
+}
