06d3a415478718b5435707b556032ad0d68d3f39
chodisettijahnavi <97425832+chodisettijahnavi@users.noreply.github.com>
2022-07-02 18:00:17 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,c=0,k=0,j,sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         if(arr[i]%2!=0)         {             for(j=i;j<n;j++)             {                 if(arr[i]==arr[j])                 {                     c++;                 }             }         }         if(c==1)         {             sum=sum+arr[i];         }     }     printf("%d",sum); }

---
+++ unique_odd_elements_sum.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,c=0,k=0,j,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        if(arr[i]%2!=0)
+        {
+            for(j=i;j<n;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    c++;
+                }
+            }
+        }
+        if(c==1)
+        {
+            sum=sum+arr[i];
+        }
+    }
+    printf("%d",sum);
+}
