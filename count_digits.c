d18529c15a70270811006f4f340c7843bf5aaaa3
chodisettijahnavi <97425832+chodisettijahnavi@users.noreply.github.com>
2022-07-02 17:41:13 +0530

    #include<stdio.h> int main() {     int i,j,n,c=0,d=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         j=a[i];         if(j==0)         {             c=1;         }         else         {             while(j)             {                 j=j/10;                 c++;             }         }         printf("%d ",c);     } }

--- count_digits.c
+++ count_digits.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n,c=0,k=0;
+    int i,j,n,c=0,d=0;
     scanf("%d",&n);
     int a[n];
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
-       // printf("%d ",a[i]);
     }
     for(i=0;i<n;i++)
     {
